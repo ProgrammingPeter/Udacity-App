2f91d54909b8d2efb90e46f078d7e17a4ae712f6
ProgrammingPeter <peter.kamaris@yahoo.com>
2015-01-22 00:09:03 -0500

    Udacity App which records and plays it back slow and fast

---
+++ Udacity App/PlaySoundsViewController.swift
@@ -0,0 +1,69 @@
+//
+//  PlaySoundsViewController.swift
+//  Udacity App
+//
+//  Created by Yosemite Retail on 1/16/15.
+//  Copyright (c) 2015 Yosemite Retail. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+
+    var MyAudioPlayer:AVAudioPlayer!
+    
+    @IBOutlet weak var stopbutton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stopbutton.hidden = true
+        
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+            
+        var filePathURL = NSURL.fileURLWithPath(filePath)
+        MyAudioPlayer = AVAudioPlayer(contentsOfURL: filePathURL, error: nil)
+            MyAudioPlayer.enableRate = true
+            
+        }
+        else{
+            println("File path was empty")
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func slowbutton(sender: UIButton) {
+        
+        stopbutton.hidden = false
+        MyAudioPlayer.stop()
+        MyAudioPlayer.rate = 0.5
+        MyAudioPlayer.play()
+    }
+
+    @IBAction func fastbutton(sender: UIButton) {
+        stopbutton.hidden = false
+        MyAudioPlayer.stop()
+        MyAudioPlayer.rate = 1.5
+        MyAudioPlayer.play()
+    }
+    @IBAction func stopsound(sender: UIButton) {
+        MyAudioPlayer.stop()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Udacity App/RecordSoundsViewController.swift
@@ -0,0 +1,76 @@
+//
+//  RecordSoundsViewController.swift
+//  Udacity App
+//
+//  Created by Yosemite Retail on 1/15/15.
+//  Copyright (c) 2015 Yosemite Retail. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController {
+
+    @IBOutlet weak var recordingprogress: UILabel!
+    @IBOutlet weak var stopbutton: UIButton!
+    @IBOutlet weak var recordbutton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    override func viewWillAppear(animated: Bool) {
+        //code
+        recordbutton.enabled = true
+        stopbutton.hidden = true
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func audiobutton(sender: UIButton) {
+        recordingprogress.hidden = false
+        recordbutton.enabled = false
+        stopbutton.hidden = false
+        
+        //Inside func recordAudio(sender: UIButton)
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String  //Specific directory in which we need to save files to
+        
+        let currentDateTime = NSDate() //Gets the current date
+        let formatter = NSDateFormatter() //Creating a var so we can format the date
+        formatter.dateFormat = "ddMMyyyy-HHmmss" //We are choosing to format the date like this
+        let recordingName = formatter.stringFromDate(currentDateTime)+".wav"
+        let pathArray = [dirPath, recordingName] //Var to have path to save and name of the file
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        println(filePath)
+        
+        var session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        
+        audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+        
+        
+        //TODO Show text "recording in progress"
+        recordingprogress.hidden = false
+        //TODO Record the user's voice
+        println("recording in progress")
+        
+    }
+
+    @IBAction func stopbutton(sender: UIButton) {
+        recordbutton.enabled = true
+        recordingprogress.hidden = true
+        audioRecorder.stop()
+        var audioSession = AVAudioSession.sharedInstance()
+        audioSession.setActive(false, error: nil)
+    }
+}
+
